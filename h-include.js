8117fe014f922d5dc6419cdb7d6b4d52c827e8f1
Gustaf Nilsson Kotte <gustaf.nk@jayway.com>
2016-05-03 21:34:18 +0200

    Add copyright line and clarify that it's an MIT license

--- h-include.js
+++ h-include.js
@@ -1,6 +1,9 @@
 /*
 h-include.js -- HTML Includes (version 1.0.0)
 
+MIT License
+
+Copyright (c) 2016 Gustaf Nilsson Kotte <gustaf.nk@gmail.com>
 Copyright (c) 2005-2012 Mark Nottingham <mnot@mnot.net>
 
 Permission is hereby granted, free of charge, to any person obtaining a copy
