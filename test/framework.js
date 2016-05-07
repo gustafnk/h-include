51f0f533e7e4d7a8d0c63f8a0e95d3b9de1d1c16
Gustaf Nilsson Kotte <gustaf.nk@jayway.com>
2016-05-07 23:55:10 +0200

    Refresh h-include element on src attribute changed

--- test/framework.js
+++ test/framework.js
@@ -28,15 +28,15 @@ function runTests(page_loc, tests, viewport) {
       console.log('BROWSER CONSOLE: ' + msg);
   };
 
-  setTimeout(function(){
-    page.open('http://localhost:' + port + '/' + page_loc, function (status) {
-      if (status === "success") {
-        console.log("testing " + port + "...");
-      } else {
-        console.error("Open problem; bailing\n");
-        phantom.exit(2);
-      }
+  page.open('http://localhost:' + port + '/' + page_loc, function (status) {
+    if (status === "success") {
+      console.log("testing " + port + "...");
+    } else {
+      console.error("Open problem; bailing\n");
+      phantom.exit(2);
+    }
 
+    setTimeout(function(){
       var i = 0;
       while (i < tests.length) {
         checkContent(tests[i][0], tests[i][1]);
@@ -44,15 +44,15 @@ function runTests(page_loc, tests, viewport) {
       }
 
       if (errors.length > 0) {
-        console.error(errors.join("\n"));
+        console.log(errors.join("\n"));
         page.render("error.png");
         phantom.exit(1);
       } else {
         console.log("Ok.");
         phantom.exit(0);
       }
-    });
-  }, 1000);
+    }, 100);
+  });
 }
 
 exports.runTests = runTests;
