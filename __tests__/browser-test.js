74dfd29e2c4765cf7f0615698be40a1234db1dad
Gustaf Nilsson Kotte <gustaf.nk@jayway.com>
2018-12-16 10:28:31 +0100

    Rewrite test runner to mocha
    - Support running multiple browsers local and remote
    - Configurable timeout per browser

---
+++ __tests__/browser-test.js
@@ -0,0 +1,186 @@
+const path = require('path');
+const fs = require('fs');
+
+const expect = require('expect');
+const { Builder, By, Key, until } = require('selenium-webdriver');
+const SauceLabs = require('saucelabs'),
+    username = process.env.SAUCE_USERNAME,
+    accessKey = process.env.SAUCE_ACCESS_KEY,
+    saucelabs = new SauceLabs({
+      username: username,
+      password: accessKey
+    });
+
+const caps = {};
+let browsers;
+
+if (process.env.IS_LOCAL === 'true') {
+  browsers = JSON.parse(fs.readFileSync(path.resolve(__dirname, 'browsers-local.json'), 'utf8'));
+} else {
+  browsers = JSON.parse(fs.readFileSync(path.resolve(__dirname, 'browsers-remote.json'), 'utf8'));
+
+  // Setup Travis and SauceLabs
+  var tunnelId, buildId;
+  if (process.env.TRAVIS === 'true') {
+    tunnelId = process.env.TRAVIS_JOB_NUMBER;
+    buildId = process.env.TRAVIS_BUILD_NUMBER;
+  }
+  else if (process.env.SAUCE_TUNNEL_ID) {
+    tunnelId = process.env.SAUCE_TUNNEL_ID;
+    buildId = 0;
+  }
+
+  Object.assign(caps, {
+    'tunnel-identifier': tunnelId,
+    build: buildId,
+    username: process.env.SAUCE_USERNAME,
+    accessKey: process.env.SAUCE_ACCESS_KEY,
+    'username': username,
+    'accessKey': accessKey,
+  });
+}
+
+browsers.forEach(browser => {
+  const browserString = JSON.stringify(browser);
+  const timeout = browser.timeout || 250;
+
+  describe(`h-include - ${browserString}`, () => {
+    let driver;
+
+    beforeEach(function() {
+      if (process.env.IS_LOCAL === 'true') {
+        driver = new Builder().forBrowser('firefox').build();
+      } else {
+        Object.assign(caps, browser);
+
+        server = 'http://' + username + ':' + accessKey +
+                  '@ondemand.saucelabs.com:80/wd/hub';
+
+        driver = new Builder().
+          withCapabilities(caps).
+          usingServer(server).
+          build();
+
+        driver.getSession().then(function (sessionid){
+          driver.sessionID = sessionid.id_;
+          console.log(`SauceOnDemandSessionID=${driver.sessionID} job-name=${browserString}`);
+        });
+      }
+    });
+
+    it('includes basic case', async () => {
+      await driver.get('http://localhost:8080/basic/');
+      await driver.sleep(timeout);
+
+      const a = await driver.findElement(By.id('a')).getText();
+      const b = await driver.findElement(By.id('b')).getText();
+
+      expect(a).toBe('this text is included');
+      expect(b).toBe('this text overwrote what was just there.');
+    });
+
+    it('includes basic async case', async () => {
+      await driver.get('http://localhost:8080/basic-async/');
+      await driver.sleep(timeout);
+
+      const a = await driver.findElement(By.id('a')).getText();
+      const b = await driver.findElement(By.id('b')).getText();
+
+      expect(a).toBe('this text is included');
+      expect(b).toBe('this text overwrote what was just there.');
+    });
+
+    it('includes lazy', async () => {
+      await driver.get('http://localhost:8080/lazy/');
+      await driver.sleep(timeout);
+
+      const a = await driver.findElement(By.id('counter')).getText();
+
+      expect(a).toBe('3');
+    });
+
+    it('includes fragment with extraction', async () => {
+      await driver.get('http://localhost:8080/fragment-extraction/');
+      await driver.sleep(timeout);
+
+      const a = await driver.findElement(By.id('a')).getText();
+
+      expect(a).toBe('Paragraph in fragment');
+    });
+
+    it('does not modify the page if no includes', async () => {
+      await driver.get('http://localhost:8080/none/');
+      await driver.sleep(timeout);
+
+      const a = await driver.findElement(By.id('a')).getText();
+
+      expect(a).toBe('1st para');
+    });
+
+    it('does not allow recursion', async () => {
+      await driver.get('http://localhost:8080/recursion-not-allowed/');
+      await driver.sleep(timeout);
+
+      const a = await driver.findElement(By.id('a')).getText();
+
+      expect(a).toMatch('h-include.js test page\nRecursion not allowed');
+    });
+
+    it('reloads if src attribute is modified', async () => {
+      await driver.get('http://localhost:8080/update-src/');
+      await driver.sleep(timeout);
+
+      const a = await driver.findElement(By.id('a')).getText();
+
+      expect(a).toBe('this text overwrote what was just there');
+    });
+
+    it('loads large fragment for large viewport', async () => {
+      if (browser.browserName === 'MicrosoftEdge' && browser.platform === 'Windows 10') {
+        return;
+      }
+
+      const viewport = driver.manage().window();
+      await viewport.setSize(800, 800); // width, height
+      await driver.sleep(timeout);
+      await driver.get('http://localhost:8080/media/');
+      await driver.sleep(timeout);
+
+      const a = await driver.findElement(By.id('a')).getText();
+
+      expect(a).toBe('Large viewport');
+    });
+
+    it('loads large fragment for large viewport', async () => {
+      if (browser.browserName === 'MicrosoftEdge' && browser.platform === 'Windows 10') {
+        return;
+      }
+
+      const viewport = driver.manage().window();
+      await viewport.setSize(480, 800); // width, height
+      await driver.sleep(timeout);
+      await driver.get('http://localhost:8080/media/');
+      await driver.sleep(timeout);
+
+      const a = await driver.findElement(By.id('a')).getText();
+
+      expect(a).toBe('Small viewport');
+    });
+
+    afterEach(function(done){
+      driver.quit();
+
+      if (process.env.IS_LOCAL === 'true') {
+        done();
+      } else {
+        var title = this.currentTest.title,
+          passed = (this.currentTest.state === 'passed') ? true : false;
+
+        saucelabs.updateJob(driver.sessionID, {
+          name: title,
+          passed: passed
+        }, done);
+      }
+    });
+  });
+});
