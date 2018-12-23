f833a67125282e776ed6de5be50f28d12305635c
Gustaf Nilsson Kotte <gustaf.nk@jayway.com>
2018-12-23 17:00:45 +0100

    Enable test case

--- __tests__/browser-test.js
+++ __tests__/browser-test.js
@@ -122,7 +122,7 @@ browsers.forEach(browser => {
       expect(a).toBe('1st para');
     });
 
-    xit('does not allow recursion', async () => {
+    it('does not allow recursion', async () => {
       await driver.get('http://localhost:8080/recursion-not-allowed/');
       await driver.sleep(timeout);
 
