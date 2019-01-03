9abb9164ac3b38fca35f3db395cfd0bc98b7a5ef
Gustaf Nilsson Kotte <gustaf.nk@jayway.com>
2019-01-03 20:31:14 +0100

    Move h-include to lib folder
    Add deprecation warning for h-include in root folder

---
+++ copy-and-deprecate.sh
@@ -0,0 +1,3 @@
+cp lib/h-include.js h-include.js
+echo "" >> h-include.js
+echo "console.warn('Using h-include.js from the root folder is deprecated, please use lib/h-include.js instead');" >> h-include.js
