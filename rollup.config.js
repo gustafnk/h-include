2056a89e78779f5a62a90d3311ed794f746b8acf
nicolasdelfino <nicolas.delfino@tretton37.com>
2019-07-21 22:41:15 +0200

    cjs plugin

--- rollup.config.js
+++ rollup.config.js
@@ -1,4 +1,5 @@
 import pkg from './package.json';
+import commonjs from 'rollup-plugin-commonjs';
 
 export default [
 	// browser-friendly UMD build
@@ -8,7 +9,10 @@ export default [
 			name: 'HInclude',
 			file: pkg.browser,
 			format: 'umd'
-		}
+		},
+		plugins: [
+			commonjs()
+		]
 	},
 	{
 		input: 'lib/h-include.js',
